229ccbf6b75ff0d49a46ca381f587ea8dece05da
lox9973 <me@lox9973.com>
2023-05-04 17:04:27 +0800

    support gpu readback in udon

--- Addon/VRCW/UdonPlayer/Script/CamRTReader.cs
+++ Addon/VRCW/UdonPlayer/Script/CamRTReader.cs
@@ -2,21 +2,44 @@ using UnityEngine;
 
 namespace ShaderMotion {
 [RequireComponent(typeof(Camera))]
-#if UDON
+#if !UDON
+public class CamRTReader : MonoBehaviour
+#else
 [UdonSharp.UdonBehaviourSyncMode(UdonSharp.BehaviourSyncMode.None)]
 public class CamRTReader : UdonSharp.UdonSharpBehaviour
-#else
-public class CamRTReader : MonoBehaviour
 #endif
 {
 	public Texture2D outputTexture;
 
 	private RenderTexture inputTexture;
+	private Color[] colors;
 	void OnEnable() {
 		inputTexture = GetComponent<Camera>().targetTexture;
+		colors = new Color[inputTexture.width*inputTexture.height];
 	}
 	void OnPostRender() {
+#if !UDON
+		UnityEngine.Rendering.AsyncGPUReadback.Request(inputTexture, 0, TextureFormat.RGBAFloat, OnAsyncGpuReadbackComplete);
+#elif UNITY_ANDROID // TODO: readback doesn't work yet
 		outputTexture.ReadPixels(new Rect(0, 0, inputTexture.width, inputTexture.height), 0, 0, false);
+#else
+		VRC.SDK3.Rendering.VRCAsyncGPUReadback.Request(inputTexture, 0, TextureFormat.RGBAFloat, (VRC.Udon.Common.Interfaces.IUdonEventReceiver)(Component)this);
+#endif
+	}
+
+#if !UDON
+	public void OnAsyncGpuReadbackComplete(UnityEngine.Rendering.AsyncGPUReadbackRequest request)
+#else
+	public void OnAsyncGpuReadbackComplete(VRC.SDK3.Rendering.VRCAsyncGPUReadbackRequest request)
+#endif
+	{
+		if (request.hasError || !request.done) return;
+#if !UDON
+		request.GetData<Color>().CopyTo(colors);
+#else
+		request.TryGetData(colors);
+#endif
+		outputTexture.SetPixels(colors);
 	}
 }
 }
